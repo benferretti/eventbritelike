5cb34a2c24df2c28d4f4f21bb9d8455d10791e61
Adèle Cailliere <adelecailliere@MacBook-Air-de-Adele.local>
2020-08-10 13:25:15 +0200

    db ok

---
+++ app/models/event.rb
@@ -0,0 +1,32 @@
+class Event < ApplicationRecord
+    has_many :attendances
+    has_many :users, through: :attendances
+
+
+
+    validates :start_date, presence: true
+    validates :duration, numericality: { greater_than: 4, less_than_or_equal_to: 30 }
+    validates :title, length: {minimum: 5, maximum: 140}, allow_blank: false
+    validates :description, length: {minimum: 20, maximum: 1000}, allow_blank: false
+    validates :price, numericality: { greater_than: 0, less_than_or_equal_to: 1000 }
+    validates :location, presence: true
+
+
+    validate :duration_multiple_of_five?
+    validate :deadline_is_possible?
+
+
+
+    def duration_multiple_of_five?
+        if (duration % 5) != 0
+        errors.add(:deadline, 'must be multiple of 5')
+        end
+    end
+
+    def deadline_is_possible?
+        if start_date < Time.now
+        errors.add(:deadline, 'must be possible')
+        end
+    end
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,52 @@
+require 'faker'
+
+
+User.destroy_all
+User.reset_pk_sequence
+Event.destroy_all
+Event.reset_pk_sequence
+Attendance.destroy_all
+Attendance.reset_pk_sequence
+
+Faker::Config.locale = 'fr'
+
+
+events = Array.new
+users = Array.new
+
+
+11.times do 
+  user = User.create(
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  email: Faker::Internet.email,
+  encrypted_password: "Azerty"
+  )
+  users << user
+  puts "Seeding User #{user.first_name} #{user.last_name}"
+end
+
+100.times do 
+    event = Event.create(
+    start_date: Faker::Date.between(from: '2020-07-23', to: '2020-09-25'),
+    title: Faker::Lorem.sentence(word_count: 2, supplemental: false, random_words_to_add: 2),
+    duration: Faker::Number.between(from: 4, to: 30),
+    description: Faker::Lorem.sentence(word_count: 8, supplemental: false, random_words_to_add: 4),
+    price: Faker::Number.between(from: 1, to: 1200),
+    location: Faker::Address.city
+    )
+    events << event
+    puts "Seeding Event #{event.id} #{event.title}"
+end
+
+200.times do
+    attendance = Attendance.create(
+      user_id: users[rand(0..10)].id,
+      event_id: events[rand(0..10)].id,
+      stripe_customer_id: Faker::Lorem.sentence(word_count: 2, supplemental: false, random_words_to_add: 2)
+      )
+      puts "Seeding Attendance #{attendance.id}"
+end
+
+
+
