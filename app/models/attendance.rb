ece2fe682c25b3a462fcb561413a569afa12a3d0
Adèle Cailliere <adelecailliere@MacBook-Air-de-Adele.local>
2020-08-12 11:01:21 +0200

    devise ok

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,7 +2,7 @@ class Attendance < ApplicationRecord
     belongs_to :event
     belongs_to :user
 
-    after_create :attendance_send
+    #after_create :attendance_send
 
   def attendance_send
     AttendanceMailer.event_admin_email(self).deliver_now
